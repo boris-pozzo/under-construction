49f97985645a1e1dfffd54204a471ba5524f6a6a
Boris <boris.pozzo@gmail.com>
2018-04-10 00:36:38 -0400

    link social media

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
           <img src={logo} className="App-logo animated rubberBand" alt="logo" />
           <h2 className="App-title">Notre site est actuellement en<br /> construction.</h2>
           <p>Patience on arrive...</p>
-          <div className="social-container">
-            <div className="social-buttons"><i className="fab fa-facebook"></i></div>
-            <div className="social-buttons"><i className="fab fa-linkedin-in"></i></div>
-            <div className="social-buttons"><i className="fab fa-twitter"></i></div>
-            <div className="social-buttons"><i className="fas fa-envelope"></i></div>
-          </div>
+          <ul className="social-container">
+            <li className="social-buttons"><a href="https://www.facebook.com/Joobla-1857027544620593/?ref=br_rs"><i className="fab fa-facebook"></i></a></li>
+            <li className="social-buttons"><a href="https://www.linkedin.com/in/team-joobla-06b494161/"><i className="fab fa-linkedin-in"></i></a></li>
+            <li className="social-buttons"><a href="https://twitter.com/joobla_fr"><i className="fab fa-twitter"></i></a></li>
+            <li className="social-buttons"><a href="mailto:contact@joobla.fr"><i className="fas fa-envelope"></i></a></li>
+          </ul>
         </div>
         <div id="footer">
           <p >Made with <i className="fas fa-heart animated bounce"></i> by <strong>Joobla</strong></p>
