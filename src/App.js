cf29eaf8ad8fb22336e15d303ea531a287a032d1
Boris <boris.pozzo@gmail.com>
2018-04-09 23:45:05 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './joobla.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="content-container">
+          <img src={logo} className="App-logo animated rubberBand" alt="logo" />
+          <h2 className="App-title">Notre site est actuellement en<br /> construction.</h2>
+          <p>Patience on arrive...</p>
+          <div className="social-container">
+            <div className="social-buttons"><i className="fab fa-facebook"></i></div>
+            <div className="social-buttons"><i className="fab fa-linkedin-in"></i></div>
+            <div className="social-buttons"><i className="fab fa-twitter"></i></div>
+            <div className="social-buttons"><i className="fas fa-envelope"></i></div>
+          </div>
+        </div>
+        <p className="footer">Made with <i className="fas fa-heart animated bounce"></i> by <strong>Joobla</strong></p>
+
+      </div >
+    );
+  }
+}
+
+export default App;
